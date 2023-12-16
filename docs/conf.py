69feeb4b9e0fea77160bd40be6a2a4ebc9dca86e
Dirk Lennarz <dirk.lennarz@web.de>
2023-12-16 12:50:18 -0800

    Run newest version of scikit-hep cookiecutter (cookiecutter gh:scikit-hep/cookie)

--- docs/conf.py
+++ docs/conf.py
@@ -1,63 +1,45 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 from __future__ import annotations
 
-# Warning: do not change the path here. To use autodoc, you need to install the
-# package first.
-
-# -- Project information -----------------------------------------------------
+import importlib.metadata
 
 project = "feldman-cousins"
-copyright = "2015 to 2023, Dirk Lennarz"
+copyright = "2023, Dirk Lennarz"
 author = "Dirk Lennarz"
+version = release = importlib.metadata.version("feldman_cousins")
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
     "sphinx_copybutton",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = []
+source_suffix = [".rst", ".md"]
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", ".env"]
+html_theme = "furo"
 
+myst_enable_extensions = [
+    "colon_fence",
+]
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_book_theme"
-
-html_title = f"{project}"
-
-html_baseurl = "https://feldman_cousins.readthedocs.io/en/latest/"
-
-html_theme_options = {
-    "home_page_in_toc": True,
-    "repository_url": "https://github.com/Scikit-HEP/feldman-cousins",
-    "use_repository_button": True,
-    "use_issues_button": True,
-    "use_edit_page_button": True,
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path: list[str] = []
+nitpick_ignore = [
+    ("py:class", "_io.StringIO"),
+    ("py:class", "_io.BytesIO"),
+]
+
+always_document_param_types = True
